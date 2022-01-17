62ceade4158dcfdf43273d7f04c9de37814ac746
AnnaYegorova <good4anna@gmail.com>
2022-01-17 16:12:38 +0200

    fix const API_key

--- src/js/components/new-api-service.js
+++ src/js/components/new-api-service.js
@@ -1,10 +1,11 @@
+const API_KEY = `25261473-89ff100c1e0e7a5a30ee5e680`;
 export default class NewsApiService {
   constructor() {
     this.searchQuery = '';
   }
   fetchArticles() {
     console.log(this);
-    const url = `https://pixabay.com/api/?key='25261473-89ff100c1e0e7a5a30ee5e680'&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=1&per_page=10`;
+    const url = `https://pixabay.com/api/?key=${API_KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=1&per_page=10`;
     fetch(url)
       .then(r => r.json())
       .then(console.log);
